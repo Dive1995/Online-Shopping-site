5cc30abadef04448a3e97e6b7369642f3839ca87
Diveshan <diveshan1995@gmail.com>
2022-04-02 10:39:42 +0530

    Uploaded new ER diagram

---
+++ ShoppingWebAPI/ShoppingWebAPI/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ShoppingWebAPI.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int PhoneNum { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ ShoppingWebAPI/ShoppingWebAPI/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ShoppingWebAPI.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+    }
+}
